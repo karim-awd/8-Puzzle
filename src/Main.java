de32db2dd7f3b51b9f3335be6c3cc4992f21bf3a
kamal <kamalhabieb@yahoo.ca>
2019-11-22 19:06:31 +0200

    BFS and DFS

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 
 public class Main {
     public static void main(String[] args){
-        //TreeNode root = new TreeNode();
+        TreeNode root;
+        SearchAlgorithms searchAlgorithms = new SearchAlgorithms();
+        //searchAlgorithms.BFS(root);
+        //searchAlgorithms.DFS(root,0);
     }
 
 }
--- src/SearchAlgorithms.java
+++ src/SearchAlgorithms.java
@@ -1,22 +1,50 @@
+import sun.reflect.generics.tree.Tree;
+
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.PriorityQueue;
 
 public class SearchAlgorithms {
-    public static void BFS(TreeNode root){
+    private final int MAXDEPTH = 100;
+    public void BFS(TreeNode root){
         ArrayList<TreeNode> frontier = new ArrayList<>();
+        root.setExploredState(true);
         frontier.add(root);
-        HashSet<TreeNode> explored = new HashSet<>();
-
+        HashSet<TreeNode> inQueue = new HashSet<>();
         while(!frontier.isEmpty()){
             TreeNode state = frontier.remove(0);
-            explored.add(state);
-            //state.setExploredState();
+            //state.print();
+            inQueue.add(state);
+            if(state.isGoalState()){
+                return;
+            }
+            if(!state.isExplored()) { //if not explored then it wasn't created before hence it is not in the queue
+                ArrayList<TreeNode> children = state.getChildren();
+                for (TreeNode child : children) {
+                    //could check if it is in queue or not
+                    frontier.add(child);
+                    inQueue.add(state);
+                }
+            }
         }
     }
-    public static void DFS(TreeNode root){
-        //Queue<TreeNode> frontier = new Que
+    public void DFS(TreeNode state, int depth){
+        if(state.isGoalState()){
+            return;
+        }
+        if(!state.isExplored()) {
+            //state.print();
+            state.setExploredState(true);
+            ArrayList<TreeNode> children = state.getChildren();
+            for (TreeNode child : children){
+                if(depth < MAXDEPTH) {
+                    DFS(child, depth + 1);
+                }
+            }
+        }
     }
     public static void A_Star(TreeNode root){
-        //Queue<TreeNode> frontier = new Que
+        PriorityQueue<TreeNode> priorityQueue = new PriorityQueue<>();
+
     }
 }

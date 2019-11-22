421bec4a48628c45c880ec494f9b4c6ab1599f07
karim-mohamed <k.mohamed3@hotmail.com>
2019-11-22 20:02:24 +0200

    combining code

--- src/CompinationsTree.java
+++
@@ -1,4 +0,0 @@
-public interface CompinationsTree {
-
-    TreeNode getTree();
-}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        TreeNode root;
         SearchAlgorithms searchAlgorithms = new SearchAlgorithms();
-        //searchAlgorithms.BFS(root);
-        //searchAlgorithms.DFS(root,0);
+
+        Scanner s = new Scanner(System.in);
+        String input = s.nextLine();
+
+        int [] intialNodeState = new int [9];
+        int emptyCellIndex = init(input, intialNodeState);
+        TreeNodeState root = new TreeNodeState(intialNodeState, emptyCellIndex);
+
+        searchAlgorithms.BFS(root);
+        searchAlgorithms.DFS(root,0);
     }
+
+    private static int init(String input, int[] intialNodeState) {
+        char[] chars = input.toCharArray();
+        int index =0;
+        int emptyCellIndex = 0;
+        for (char ch : chars){
+            if (ch >= '0' && ch <='8'){
+                if (ch == '0'){
+                    emptyCellIndex = index;
+                }
+                intialNodeState[index++] = Integer.parseInt(""+ ch);
+            }
+        }
+        return emptyCellIndex;
+    }
+
 
 }
